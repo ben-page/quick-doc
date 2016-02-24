378e5bb9fa5a5730afdf450153311e26d6bea678
Ben Page <ben.page@openreign.com>
2016-02-24 14:18:13 -0600

    handled single line block comments

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 'use strict';
 var util = require('util');
 
-//extra comment
-
+//multiple comments
+/*
+    that reference the same line
+        respect indention
+ */
 /**
  * JSDOC comment
  * @param object
@@ -11,17 +14,15 @@ function test(object) {
     //line comment
     console.log(util.inspect(object));
 
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 100 /* embedded block comment */; i++) {
         console.log(i); //embedded line comment
     }
 
     var t = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-    //even if
-    //they push
-    //down the
-    //rest of
-    //the code
+    //multiple
+    //line
+    //comments
     for (var f = 0; f < 100; f++) {
         console.log(f);
     }
